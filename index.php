586c30ff834a535ac11b73b91b4c5036d29736fc
Uli Kusterer <ulivc@zathras.de>
2011-03-12 14:12:40 +0100

    You can view the tweet RSS feed for a user without logging in now.

--- index.php
+++ index.php
@@ -1,8 +1,38 @@
 <?php
-// split the user/pass parts
-if( !isset($_SERVER['PHP_AUTH_USER']) )
+// -----------------------------------------------------------------------------
+//  Set up some commonly needed stuff:
+// -----------------------------------------------------------------------------
+
+$configpath = dirname($_SERVER['SCRIPT_FILENAME']).'/config/config.ini';
+$feeddirectory = dirname($_SERVER['SCRIPT_FILENAME']).'/tweets';
+$feedpath = $feeddirectory.'/feed.rss';
+$feedidpath = $feeddirectory.'/feedid.txt';
+
+// Read prefs:
+$config = parse_ini_file( $configpath );
+date_default_timezone_set( $config['TIMEZONE'] );
+
+// -----------------------------------------------------------------------------
+//  Show RSS feed that contains all tweets, if desired (this is what subscribers poll):
+// -----------------------------------------------------------------------------
+if( isset($_REQUEST['format']) && $_REQUEST['format'] == "rss" )
 {
-	list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6)));
+	if( file_exists( $feedpath ) )
+	{
+		$fd = fopen( $feedpath, "r" );
+		fpassthru( $fd );
+		fclose( $fd );
+	}
+	exit;  // Done, don't go into auth area.
+}
+
+// -----------------------------------------------------------------------------
+//  Authenticate the user:
+// -----------------------------------------------------------------------------
+
+if( !isset($_SERVER['PHP_AUTH_USER']) ) // Likely that we need PHP CGI workaround for HTTP AUTH?
+{
+	list($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']) = explode(':', base64_decode(substr($_SERVER['HTTP_AUTHORIZATION'], 6))); // Get user/pw from environment and put them in the array.
 	if( strlen($_SERVER['PHP_AUTH_USER']) == 0 || strlen($_SERVER['PHP_AUTH_PW']) == 0 )
 	{
 		unset($_SERVER['PHP_AUTH_USER']);
@@ -10,7 +40,7 @@ if( !isset($_SERVER['PHP_AUTH_USER']) )
 	}
 }
 
-// open a user/pass prompt
+// open a user/pass prompt:
 if( !isset($_SERVER['PHP_AUTH_USER']) )
 {
 	header('WWW-Authenticate: Basic realm="My Realm"');
@@ -18,15 +48,8 @@ if( !isset($_SERVER['PHP_AUTH_USER']) )
 	echo 'Text to send if user hits Cancel button';
 	exit;
 }
-else
+else    // Have a username/password? Compare to config file:
 {
-	$configpath = dirname($_SERVER['SCRIPT_FILENAME']).'/config/config.ini';
-	$feeddirectory = dirname($_SERVER['SCRIPT_FILENAME']).'/tweets';
-	$feedpath = $feeddirectory.'/feed.rss';
-	$feedidpath = $feeddirectory.'/feedid.txt';
-	
-	$config = parse_ini_file( $configpath );
-   
 	if( strtolower($_SERVER['PHP_AUTH_USER']) != $config['USER']
    		|| strtolower($_SERVER['PHP_AUTH_PW']) != $config['PASS'] )
 	{
@@ -37,7 +60,9 @@ else
 	}
 }
 
-date_default_timezone_set( $config['TIMEZONE'] );
+// *****************************************************************************
+// * * * * * * * Below this we're guaranteed to be a valid login : * * * * * * *
+// *****************************************************************************
 
 $htmlusername = htmlentities($_SERVER['PHP_AUTH_USER']);
 $feedurl = $_SERVER['SCRIPT_URI'];
@@ -47,11 +72,16 @@ fwrite($fd,$tweetnum);
 fclose($fd);
 $currentdate = date( DATE_RSS, time() );
 
+
+// -----------------------------------------------------------------------------
+//  User wants to post a tweet:
+// -----------------------------------------------------------------------------
+
 if( isset($_REQUEST['message']) )
 {
 	$htmlmessage = htmlentities($_REQUEST['message']);
 	
-	if( !file_exists( $feedpath ) )
+	if( !file_exists( $feedpath ) )    // First tweet? Make file!
 	{
 		$feedbody = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>
 <rss version=\"2.0\">
@@ -77,7 +107,7 @@ if( isset($_REQUEST['message']) )
 		fwrite( $fd, $feedbody );
 		fclose( $fd );
 	}
-	else
+	else   // Already have tweets? Insert item at top!
 	{
 		$tweets = file_get_contents($feedpath);
 		
@@ -97,18 +127,12 @@ if( isset($_REQUEST['message']) )
 		fclose( $fd );
 	}
 }
-else if( isset($_REQUEST['format']) && $_REQUEST['format'] == "rss" )
-{
-	if( file_exists( $feedpath ) )
-	{
-		$fd = fopen( $feedpath, "r" );
-		fpassthru( $fd );
-		fclose( $fd );
-	}
-	exit;
-}
 
-echo "<p>Hello, ".htmlentities($_SERVER['PHP_AUTH_USER'])." what's happening?</p>\n";
+// -----------------------------------------------------------------------------
+//  Display user's twitter home page:
+// -----------------------------------------------------------------------------
+
+echo "<p>Hello, ".$htmlusername." what's happening?</p>\n";
 echo "<form method=\"post\" action=\"index.php\">";
 echo "<p><input name=\"message\" type=\"text\"></p>\n";
 echo "<p><input type=\"submit\" name=\" Post \"></p>\n";
