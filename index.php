da3444e0d8bd567d209fbc8be66b6e4a7a8153c6
Uli Kusterer <ulivc@zathras.de>
2012-09-09 01:58:13 +0200

    Oops. The code to auto-install prevented installation from completing

--- index.php
+++ index.php
@@ -113,11 +113,14 @@
 	$action = str_replace("\r", "", $action);
 	
 	// Have we been set up yet?
-	$result = mysql_query( "SELECT id FROM users" );
-	if( mysql_errno() != 0 )	// Don't have a users table?	// +++ What if DB server is down? Wouldn't want to install again!
-		$action = 'install';
-	else if( mysql_num_rows( $result ) == 0 )	// Or there are no users in it?
-		$action = 'install';
+	if( strcmp($action,"install") != 0 && strcmp($action,"finish_install") != 0 )
+	{
+		$result = mysql_query( "SELECT id FROM users" );
+		if( mysql_errno() != 0 )	// Don't have a users table?	// +++ What if DB server is down? Wouldn't want to install again!
+			$action = 'install';
+		else if( mysql_num_rows( $result ) == 0 )	// Or there are no users in it?
+			$action = 'install';
+		}
 	
 	require("format_$format.inc.php");
 	require("action_$action.inc.php");
