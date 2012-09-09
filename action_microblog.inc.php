316f87b7f7f259e436d4fdbd7b7ee7fd0aaea255
Uli Kusterer <ulivc@zathras.de>
2012-09-09 08:07:21 +0200

    WIP on making it possible to run one script for several users/domains
    We use the host name to decide which account's statuses to vend via the microblog.rss feed. Should allow pointing several domains to the same script/database and share the cached external accounts' statuses.

--- action_microblog.inc.php
+++ action_microblog.inc.php
@@ -1,8 +1,5 @@
 <?php
-	$result = mysql_query ("SELECT shortname FROM users WHERE isAdmin=1 LIMIT 1");
-	print_r( mysql_error() );
-	$row = mysql_fetch_assoc($result);
-	$_REQUEST['shortname'] = $row['shortname'];
+	$_REQUEST['shortname'] = $_SERVER['HTTP_HOST'];
 	
 	require("action_home.inc.php");
 ?>
