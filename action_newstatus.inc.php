74b387b91bf674baa3c1fed0c359e2ac76ecdab9
Uli Kusterer <ulivc@zathras.de>
2012-09-23 01:09:04 +0200

    Add UI for actually reposting a status.

--- action_newstatus.inc.php
+++ action_newstatus.inc.php
@@ -8,6 +8,7 @@
 		$text = mysql_real_escape_string($_REQUEST['text']);
 		
 		$inreplyto = $_REQUEST['statusid'];
+		$original = $_REQUEST['originalstatusid'];
 		if( isset($inreplyto) && strlen($inreplyto) > 0 && is_numeric($inreplyto) )
 		{
 			$result = mysql_query( "SELECT * FROM statuses WHERE id='$inreplyto'" );
@@ -19,10 +20,26 @@
 				$inreplyto = mysql_real_escape_string("http://".$_SERVER['HTTP_HOST']."/chirp/index.php?statusid=".$_REQUEST['statusid']);
 		}
 		else
+		if( isset($original) && strlen($original) > 0 && is_numeric($original) )
+		{
+			$result = mysql_query( "SELECT * FROM statuses WHERE id='$original'" );
+			print_r( mysql_error() );
+			$row = mysql_fetch_assoc($result);
+			
+			$originaluserid = mysql_real_escape_string($row['original_user_id']);
+			$inreplyto = mysql_real_escape_string($row['url']);
+			if( strlen($original) == 0 )
+				$original = mysql_real_escape_string("http://".$_SERVER['HTTP_HOST']."/chirp/index.php?statusid=".$_REQUEST['statusid']);
+		}
+		else
+		{
 			$inreplyto = '';
+			$original = '';
+			$originaluserid = 0;
+		}
 		$time = time();
 		$url = "http://".$_SERVER['HTTP_HOST']."/chirp/index.php?statusid=";
-		$result = mysql_query( "INSERT INTO statuses VALUES ( NULL, '$userid', '$inreplyto', '$text', '$url', $time )" );
+		$result = mysql_query( "INSERT INTO statuses VALUES ( NULL, '$userid', '$inreplyto', '$text', '$url', $time, '$original', '$originaluserid' )" );
 		$id = mysql_insert_id();
 		$url .= $id;
 		$result = mysql_query( "UPDATE statuses SET url='$url' WHERE id='$id'" );
---
+++ action_repost.inc.php
@@ -0,0 +1,20 @@
+<?php
+	$userid = http_authenticated_userid(true);
+	if( $userid === false )
+		return;
+	
+	$statusid = $_REQUEST['statusid'];
+	if( !isset($statusid) || strlen($statusid) == 0 || !is_numeric($statusid) )
+		return;
+	$result = mysql_query( "SELECT * FROM statuses WHERE id='$statusid'" );
+	print_r( mysql_error() );
+
+	$row = mysql_fetch_assoc($result);
+	
+	$userinfo = userinfo_from_userid( $row['user_id'] );
+	
+	$str = "<form action=\"index.php\" method=\"POST\">Repost <i>".htmlentities($userinfo['fullname'])."</i>:<br />".htmlentities($row['text'])."<br/>\n";
+	$str .= "<input type=\"hidden\" name=\"text\" size=\"60\" value=\"RP ".htmlentities($userinfo['shortname']).htmlentities($row['text'])." \" /><input type=\"hidden\" name=\"action\" value=\"newstatus\"><input type=\"hidden\" name=\"originalstatusid\" value=\"".$row['id']."\"></form>";
+	
+	echo make_header() . $str . make_footer();
+?>
